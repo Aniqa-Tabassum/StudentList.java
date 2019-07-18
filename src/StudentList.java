cd88403c00d9e0f3bf0d3cd18ce796a2d6fd6d78
Aniqa-Tabassum <tabassum.fahmidah06@gmail.com>
2019-07-18 01:29:18 -0700

    Application now terminates early if the number of arguments passed into nit is wrong ,fix it

--- src/StudentList.java
+++ src/StudentList.java
@@ -5,36 +5,48 @@ public class StudentList {
 	public static void main(String[] args) {
 
 //		Check arguments
-		if(args[0].equals("Show")) {
-			System.out.println("Show student list");			
+
+        System.out.println("1. Show  student list ");
+        System.out.println("2. Show random student");
+        System.out.println("3. Add student number");
+        System.out.println("4. Search a student");
+
+       System.out.println("5. Search word number");
+
+        System.out.println("Enter Option (1-5):");
+            Scanner obj = new Scanner(System.in);
+
+        int Option = obj.nextInt();
+		if(Option == 1) {
+			System.out.println("Show student list");
 			try {
 			BufferedReader st = new BufferedReader(
                                 new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+							new FileInputStream("students.txt")));
 			String r = st.readLine();
-			String i[] = r.split(",");			
+			String i[] = r.split(",");
 			for(String j : i) { System.out.println(j); }
-			} catch (Exception e){} 
+			} catch (Exception e){}
 			System.out.println("End");
 		}
-		else if(args[0].equals("Search")) 
+		else if(Option == 2)
 		{
-			System.out.println("Enter student identity:");			
+			System.out.println("Enter student identity:");
 			try {
 			BufferedReader s = new BufferedReader(
 					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+							new FileInputStream("students.txt")));
 			String r = s.readLine();
 			System.out.println(r);
-			String i[] = r.split(",");	
-			Random x = new Random();
-				int y = x.nextInt();
+			String i[] = r.split(",");
+			Random x = new Random				int y = x.nextInt();
 					System.out.println(i[y]);
-			} catch (Exception e){} 
-			System.out.println("End");			
+			} catch (Exception e){}
+			System.out.println("End");
 		}
-		else if(args[0].contains("Add")){
-			System.out.println("Add student number:");			
+		else if(Option == 3) {();
+
+			System.out.println("Add student number:");
 			try {
 			BufferedWriter st = new BufferedWriter(
 					new FileWriter("students.txt", true));
@@ -46,18 +58,18 @@ public class StudentList {
 			st.write(", "+t+"\nList last updated on "+fd);
 			st.close();
 			} catch (Exception e){}
-							
-			System.out.println("End");	
+
+			System.out.println("End");
 		}
-		else if(args[0].contains("Search")) 
+		else if(Option == 4)
 		{
-			System.out.println("Search student:");			
+			System.out.println("Search student:");
 			try {
 			BufferedReader st = new BufferedReader(
 					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+							new FileInputStream("students.txt")));
 			String r = st.readLine();
-			String i[] = r.split(",");	
+			String i[] = r.split(",");
 			boolean done = false;
 			String t = args[0].substring(1);
 			for(int idx = 0; idx<i.length && !done; idx++) {
@@ -66,30 +78,30 @@ public class StudentList {
 						done=true;
 				}
 			}
-			} catch (Exception e){} 
-			System.out.println("End");				
+			} catch (Exception e){}
+			System.out.println("End");
 		}
-		else if(args[0].contains("Search_word")) 
+		else if(Option == 5)
 		{
-			System.out.println("Search word number:");			
+			System.out.println("Search word number:");
 			try {
 			BufferedReader s = new BufferedReader(
 					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+							new FileInputStream("students.txt")));
 			String D = s.readLine();
-			char a[] = D.toCharArray();			
+			char a[] = D.toCharArray();
 			boolean in_word = false;
 			int count=0;
 			for(char c:a) {
-				if(c ==' ') 
+				if(c ==' ')
 				{
 					if (!in_word) {	count++; in_word =true;	}
-					else { in_word=false;}			
+					else { in_word=false;}
 				}
 			}
 			System.out.println(count +" word(s) found " + a.length);
-			} catch (Exception e){} 
-			System.out.println("End");				
+			} catch (Exception e){}
+			System.out.println("End");
 		}
 	}
 }
