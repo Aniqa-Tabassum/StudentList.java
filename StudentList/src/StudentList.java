360585c68b3d4c79141e077659f1403bf8a7ea9a
Aniqa-Tabassum <tabassum.fahmidah06@gmail.com>
2019-07-18 00:26:06 -0700

    Update code style for better consistency

---
+++ StudentList/src/StudentList.java
@@ -0,0 +1,13 @@
+
+public class StudentList {
+    public static void main(String[] args) {
+        
+        System.out.println("Loadind data ...");
+         System.out.println("Studen1");
+         System.out.println("Studen2");
+         System.out.println("Studen3");
+         System.out.println("Student4");
+         System.out.println("Data Loaded");
+            
+    }
+}
